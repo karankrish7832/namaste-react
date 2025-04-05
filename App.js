d3571a39d1a08951d94c678aae3535736548e4b1
Kirubakaran <karankrish3497@gmail.com>
2025-04-06 01:46:20 +0530

    Episode-02 | Igniting our App

--- App.js
+++ App.js
@@ -1,34 +1,18 @@
-// React create element create the Object => then it's HTML element
-
-// const heading = React.createElement("h1", {id: "heading"}, "Hello World from React!");
-
-// const parent = React.createElement("div", {id: "parent"}, 
-//     React.createElement("div", {id: "child"}, 
-//         React.createElement("h1", {}, "I'm a header!")
-//     )
-// )
-
-// const parent = React.createElement("div", {id: "parent"}, 
-//     React.createElement("div", {id: "child"}, 
-//         [
-//             React.createElement("h1", {}, "I'm a h1 tag!"),
-//             React.createElement("h2", {}, "I'm a h2 tag!")
-//         ]
-//     )
-// )
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", {id: "parent"}, 
     [
-        React.createElement("div", {id: "child1"}, 
+        React.createElement("div", {id: "child1", key: "1"}, 
             [
-                React.createElement("h1", {}, "I'm a h1 tag!"),
-                React.createElement("h2", {}, "I'm a h2 tag!")
+                React.createElement("h1", {key: "1.1"}, "I'm a h1 tag!"),
+                React.createElement("h2", {key: "1.2"}, "I'm a h2 tag!")
             ]
         ),
-        React.createElement("div", {id: "child2"}, 
+        React.createElement("div", {id: "child2", key: "2"}, 
             [
-                React.createElement("h3", {}, "I'm a h3 tag!"),
-                React.createElement("h4", {}, "I'm a h4 tag!")
+                React.createElement("h3", {key: "2.1"}, "I'm a h3 tag!"),
+                React.createElement("h4", {key: "2.2"}, "I'm a h4 tag!")
             ]
         )
     ]
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Namaste React 
+
+# Parcel
+- Dev Build
+- local Server
+- HMR - Hot Module Replacement
+- File Watching Algorithm - written in c++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- consistent Hashing
+- Code Splitting
+- Different Bundling - support old browser
+- Diagnostics
+- Error Handling
+- HTTPs
+- Tree Shaking - Removed unused code
+- Different dev and prod build
